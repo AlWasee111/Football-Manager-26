f3202ab3a375832368cf8d0269947a5836f0b15b
AlWasee111 <2405113@ugrad.cse.buet.ac.bd>
2026-02-20 01:30:47 +0600

    Styled

--- src/main/java/com/footballmanager/footballmanager/MainApplication.java
+++ src/main/java/com/footballmanager/footballmanager/MainApplication.java
@@ -3,16 +3,24 @@ package com.footballmanager.footballmanager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("mainmenu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 650);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/Styles.css")).toExternalForm());
+
+        Image mainBackground = new Image("night-view-football-stadium.jpg");
+        Image icon = new Image("icon.jpg");
+
+        stage.getIcons().add(icon);
+        stage.setTitle("Football Manager 26");
         stage.setScene(scene);
         stage.show();
     }
