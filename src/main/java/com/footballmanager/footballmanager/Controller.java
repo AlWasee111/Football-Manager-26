cda7ab9cfaa765cfc3ee1b93ec98e95efa14d486
AlWasee111 <2405113@ugrad.cse.buet.ac.bd>
2026-02-20 16:38:50 +0600

    Login page

--- src/main/java/com/footballmanager/footballmanager/Controller.java
+++ src/main/java/com/footballmanager/footballmanager/Controller.java
@@ -1,8 +1,12 @@
 package com.footballmanager.footballmanager;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class Controller {
-    
+    void login (ActionEvent e) {
+        
+
+    }
 }
