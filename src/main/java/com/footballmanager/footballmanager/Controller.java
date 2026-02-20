447e18e77f4ccec48d95b8ea2cdf766fc282392a
AlWasee111 <2405113@ugrad.cse.buet.ac.bd>
2026-02-21 00:53:59 +0600

    Login

--- src/main/java/com/footballmanager/footballmanager/Controller.java
+++ src/main/java/com/footballmanager/footballmanager/Controller.java
@@ -1,12 +1,50 @@
 package com.footballmanager.footballmanager;
 
+import java.io.IOException;
+import java.util.Objects;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class Controller {
-    void login (ActionEvent e) {
-        
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
+    public void LoginScreen (ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/Styles.css")).toExternalForm());
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void SignupScreen (ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/Styles.css")).toExternalForm());
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void ReturnToMenu (ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainmenu.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/Styles.css")).toExternalForm());
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
