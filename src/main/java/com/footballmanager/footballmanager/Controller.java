9241064dfd8d17428ad81c9966ed4c08fb6b6f55
AlWasee111 <2405113@ugrad.cse.buet.ac.bd>
2026-02-21 12:45:56 +0600

    Changed Login Message

--- src/main/java/com/footballmanager/footballmanager/Controller.java
+++ src/main/java/com/footballmanager/footballmanager/Controller.java
@@ -19,7 +19,12 @@ public class Controller {
     private Parent root;
 
     public void LoginScreen (ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        root = loader.load();
+
+        LoginController controller = loader.getController();
+        controller.ChangeText("Login");
+
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/Styles.css")).toExternalForm());
@@ -29,21 +34,16 @@ public class Controller {
     }
 
     public void SignupScreen (ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        root = loader.load();
+
+        LoginController controller = loader.getController();
+        controller.ChangeText("Signup");
+
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/Styles.css")).toExternalForm());
         
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void ReturnToMenu (ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainmenu.fxml")));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/Styles.css")).toExternalForm());
-
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/footballmanager/footballmanager/LoginController.java
@@ -0,0 +1,37 @@
+package com.footballmanager.footballmanager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class LoginController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    Button EnterButton;
+
+    public void ChangeText(String message) {
+        EnterButton.setText(message);
+    }
+
+    public void ReturnToMenu (ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainmenu.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/Styles.css")).toExternalForm());
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
