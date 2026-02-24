dfbc162448836de88fb552e81aae76f13c0caa1b
AlWasee111 <2405113@ugrad.cse.buet.ac.bd>
2026-02-24 23:35:40 +0600

    Club name check

--- src/main/java/com/footballmanager/footballmanager/LoginController.java
+++ src/main/java/com/footballmanager/footballmanager/LoginController.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,11 +16,15 @@ import java.util.Objects;
 
 public class LoginController {
 
+    @FXML
+    private TextField ClubName;
+
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void ReturnToMenu (ActionEvent event) throws IOException {
+    // Returns to main menu if back button is clicked
+    public void ReturnToMenu(ActionEvent event) throws IOException {
         root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainmenu.fxml")));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -27,5 +32,25 @@ public class LoginController {
 
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void EnterPage() {
+        // Validate if club exists
+        String clubName = ClubName.getText();
+        if (!ClubExists(clubName))
+            System.out.println("Club does not exist!");
+    }
+
+
+    //Method to check if a club exists
+    private boolean ClubExists(String clubName) {
+        if (clubName.equalsIgnoreCase("Barcelona") || clubName.equalsIgnoreCase("Real Madrid") ||
+                clubName.equalsIgnoreCase("Arsenal") || clubName.equalsIgnoreCase("Bayern") ||
+                clubName.equalsIgnoreCase("Chelsea") || clubName.equalsIgnoreCase("PSG") ||
+                clubName.equalsIgnoreCase("Manchester United") || clubName.equalsIgnoreCase("Manchester City")) {
+            return true;
+        }
+        
+        return false;
     }
 }
