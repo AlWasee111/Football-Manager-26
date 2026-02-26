290ed8707ab9a426eb5198f7f2b5a7d7c63481c7
AlWasee111 <2405113@ugrad.cse.buet.ac.bd>
2026-02-26 23:57:15 +0600

    Used combobox for club selection

--- src/main/java/com/footballmanager/footballmanager/LoginController.java
+++ src/main/java/com/footballmanager/footballmanager/LoginController.java
@@ -3,6 +3,7 @@ package com.footballmanager.footballmanager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,20 +14,29 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable {
 
-    @FXML
-    private TextField ClubName;
     @FXML
     private TextField Password;
     @FXML
     private Label errorMessage;
+    @FXML
+    private ComboBox<String> myCombobox;
 
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    private final String[] clubs = {"Barcelona", "Arsenal", "Chelsea", "Manchester United",
+            "Real Madrid", "Bayern", "PSG", "Manchester City"};
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        myCombobox.getItems().addAll(clubs);
+    }
 
     // Returns to main menu if back button is clicked
     public void ReturnToMenu(ActionEvent event) throws IOException {
@@ -40,10 +50,11 @@ public class LoginController {
     }
 
     public void EnterPage(ActionEvent event) throws IOException {
-        String clubName = ClubName.getText().trim();
+//        String clubName = ClubName.getText().trim();
+        String clubName = myCombobox.getValue();
         String pass = Password.getText();
         // Validate if club exists
-        if (clubName.isEmpty()) {
+        if (clubName == null) {
             errorMessage.setText("Club name cannot be empty!");
             errorMessage.setVisible(true);
         }
@@ -69,13 +80,14 @@ public class LoginController {
 
     // Method to check if a club exists
     private boolean ClubExists(String clubName) {
-        if (clubName.equalsIgnoreCase("Barcelona") || clubName.equalsIgnoreCase("Real Madrid") ||
-                clubName.equalsIgnoreCase("Arsenal") || clubName.equalsIgnoreCase("Bayern") ||
-                clubName.equalsIgnoreCase("Chelsea") || clubName.equalsIgnoreCase("PSG") ||
-                clubName.equalsIgnoreCase("Manchester United") || clubName.equalsIgnoreCase("Manchester City")) {
+        if (clubName.equals("Barcelona") || clubName.equals("Real Madrid") ||
+                clubName.equals("Arsenal") || clubName.equals("Bayern") ||
+                clubName.equals("Chelsea") || clubName.equals("PSG") ||
+                clubName.equals("Manchester United") || clubName.equals("Manchester City")) {
             return true;
         }
 
         return false;
     }
+
 }
